25478f8c47d1296cf22f2a235c33a212845dafa5
root <damianemerah1@gmail.com>
2023-09-07 15:59:18 -0700

    redis client;

---
+++ utils/redis.js
@@ -0,0 +1,50 @@
+import { createClient } from 'redis';
+
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.error(err);
+    })
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, reply) => {
+        if (error) {
+	  reject(error);
+	} else {
+	  resolve(reply);
+	}
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) reject(err);
+	else resolve('OK');
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) reject(err);
+	else resolve(reply);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
